e9c35a9f65e63255585bbf1283581e17a6c7b38a
woshiren <yejiacong@outlook.com>
2024-06-29 21:28:58 +0800

    双控开关接入NVBOARD

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -1,32 +1,22 @@
 #include "Vtop.h"
+#include "nvboard.h"
 #include "verilated.h"
 #include "verilated_vcd_c.h"
 #include <cassert>
 #include <cstdlib>
 
+void nvboard_bind_all_pins(TOP_NAME *top);
+
 int main(int argc, char *argv[]) {
-  auto contextp = std::make_unique<VerilatedContext>();
-  contextp->commandArgs(argc, argv);
-  auto top = std::make_unique<Vtop>(contextp.get());
+  TOP_NAME top;
+  nvboard_bind_all_pins(&top);
+  nvboard_init();
 
-  Verilated::traceEverOn(true);
-  auto tfp = std::make_unique<VerilatedVcdC>();
-  top->trace(tfp.get(), 99);
-  tfp->open("top.vcd");
-
-  int sim_time = 100;
-
-  while (contextp->time() < sim_time && !contextp->gotFinish()) {
-    contextp->timeInc(1);
-    int a = rand() & 1;
-    int b = rand() & 1;
-    top->a = a;
-    top->b = b;
-    top->eval();
-    printf("a=%d, b=%d, f=%d\n", a, b, top->f);
-    assert(top->f == (a ^ b));
-    tfp->dump(contextp->time());
+  while (1) {
+    nvboard_update();
+    top.eval();
   }
-  tfp->close();
+
+  nvboard_quit();
   return 0;
 }

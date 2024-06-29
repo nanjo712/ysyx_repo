a9f4241cc72ef40bf90448994409d7c3c4a099c7
woshiren <yejiacong@outlook.com>
2024-06-29 20:28:51 +0800

    NJU-ProjectN/nvboard master initialized without tracing
    NJU-ProjectN/nvboard a48416ef8179faa6e40df1355cd9c613af8382f6 fix compile error with clang in MacOS

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -1,6 +1,32 @@
-#include <stdio.h>
+#include "Vtop.h"
+#include "verilated.h"
+#include "verilated_vcd_c.h"
+#include <cassert>
+#include <cstdlib>
 
-int main() {
-  printf("Hello, ysyx!\n");
+int main(int argc, char *argv[]) {
+  auto contextp = std::make_unique<VerilatedContext>();
+  contextp->commandArgs(argc, argv);
+  auto top = std::make_unique<Vtop>(contextp.get());
+
+  Verilated::traceEverOn(true);
+  auto tfp = std::make_unique<VerilatedVcdC>();
+  top->trace(tfp.get(), 99);
+  tfp->open("top.vcd");
+
+  int sim_time = 100;
+
+  while (contextp->time() < sim_time && !contextp->gotFinish()) {
+    contextp->timeInc(1);
+    int a = rand() & 1;
+    int b = rand() & 1;
+    top->a = a;
+    top->b = b;
+    top->eval();
+    printf("a=%d, b=%d, f=%d\n", a, b, top->f);
+    assert(top->f == (a ^ b));
+    tfp->dump(contextp->time());
+  }
+  tfp->close();
   return 0;
 }
